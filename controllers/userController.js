4eb3c644a43851f14c632351e96059d260c7d9bc
Adarsh28dixit <adarsh@noapp.io>
2023-04-06 12:27:08 +0530

    minor changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,5 +50,16 @@ const signinUser = async(req, res) => {
     }
 }
 
+const logOutUser = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.clearCookie('session_id');
+      res.status(200).json({ message: 'Logged out successfully' });
+    }
+  });
+};
 
-module.exports = {getUsers, createUser, signinUser};
+module.exports = {getUsers, createUser, signinUser,logOutUser};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 var express = require('express');
-const { getUsers, createUser, signinUser } = require('../controllers/userController');
+const { getUsers, createUser, signinUser, logOutUser } = require('../controllers/userController');
 var router = express.Router();
 
 router.get('/getUsers', getUsers);
 router.post('/createUser', createUser);
 // router.get('/getUserById/:id', getUserById)
 router.post('/signinUser', signinUser)
+router.post('/logout', logOutUser)
 
 module.exports = router;
